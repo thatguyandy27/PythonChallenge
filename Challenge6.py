dd99b5c2e268f3b1f0c791e89b01776ab2cf8e78
Andy M <andy.dude@gmail.com>
2013-05-20 20:17:44 -0500

    Finished Challenge 6

--- Challenge6.py
+++ Challenge6.py
@@ -2,6 +2,7 @@
 from ChallengeBase import ChallengeBase
 import urllib2
 import re
+import pickle
 
 
 class Challenge6(ChallengeBase):
@@ -9,7 +10,31 @@ class Challenge6(ChallengeBase):
     def __init__(self):
         super(Challenge6, self).__init__(
             'http://www.pythonchallenge.com/pc/def/peak.html')
+        self.bannerPage = 'http://www.pythonchallenge.com/pc/def/banner.p'
+
+    def getNextChallengeUrl(self):
+        return 'http://www.pythonchallenge.com/pc/def/channel.html'
+
+    def getBannerPage(self):
+        response = urllib2.urlopen(self.bannerPage)
+        content = response.read()
+
+        pickleList = pickle.loads(content)
+        for l in pickleList:
+            listString = ''
+            # print l
+            # for t in l:
+            print ''.join(map(lambda pair: pair[0] * pair[1], l))
+                # listString += t[0] + str(t[1])
+                # if t[0] == ' ':
+                    # listString += chr(t[1])
+                # else:
+                    # listString += str(t[1])
+
+            print listString
+
 
 if __name__ == '__main__':
     challenge = Challenge6()
     challenge.openNextChallengePage()
+    # challenge.getBannerPage()
